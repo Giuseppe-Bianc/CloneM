02c9c618a7d0a7620994e490748894d6fe2b973e
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2022-10-19 18:46:42 +0200

    agg

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
-package org.dersbian;
+import jade.Window;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Window window = Window.get();
+        window.run();
     }
 }
